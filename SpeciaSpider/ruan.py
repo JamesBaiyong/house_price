9fda10d89b9d930b4a0e4752f052a8d321631974
JamesBaiyong <mrbaiyongde@sina.com>
2018-02-06 16:29:01 +0800

    fix

--- SpeciaSpider/ruan.py
+++
@@ -1,4 +0,0 @@
-#coding=utf-8
-from scrapy.cmdline import execute
-# execute(['scrapy','crawl','NeiMengHuanBao','-o ******.csv']) #直接输出CSV格式，office打开会有乱码
-execute(['scrapy','crawl','NeiMengHuanBao'])
--- YueDu/main.py
+++
@@ -1,3 +0,0 @@
-from scrapy.cmdline import execute
-execute(['scrapy','crawl','YueDuSpider'])
-# execute(['scrapy','crawl','YueDuSpider','-o' 'data.csv'])
