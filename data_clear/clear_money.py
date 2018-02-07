0580a6a816f6835620734bd5d6978fc3e0899455
JamesBaiyong <mrbaiyongde@sina.com>
2018-02-07 19:16:13 +0800

    对房价信息重新清洗

--- data_clear/clear_money.py
+++ data_clear/clear_money.py
@@ -12,7 +12,7 @@ class ClearMoney(object):
     def __init__(self):
         self.csv_file = open('./data_table/house_price.csv','w')
         self.writers = csv.writer(self.csv_file)
-        self.writers.writerow(['city_name', 'house_price_list'])
+        self.writers.writerow(['city_name', 'house_price_list','mean'])
         self.tables = [
             'zhongshan',
             'chongqing',
@@ -38,41 +38,63 @@ class ClearMoney(object):
         house_area = df.area.values
         # 提取出房价列
         house_price = df.price.values
-        print(len(house_price))
         # 提取出房类型
         house_type = df.type
         # 提取地方
         house_addres = df.position
         # 楼盘名
         house_name = df.name.values
-        house_price_list = self.extract_money(house_price, house_area, house_name)
+        house_price_list = self.extract_money(house_price)
+        mean = sum(house_price_list) / len(house_price_list)
+
         try:
-            self.writers.writerow([city_name,house_price_list])
+            self.writers.writerow([city_name,house_price_list,mean])
         except:
             self.csv_file.close()
+        house_price_area = self.extract_money_area(city_name,house_price,house_area)
 
-    def extract_money(self,house_price, house_area, house_name):
-        # 清洗房价的格式,包括计算超预期数据
+
+    def extract_money(self,house_price):
+        # 清洗房价的格式,去除影响房价信息
         money_list = []
         # money_dict = defaultdict(list)
         house_info = len(house_price)
         for item in range(house_info):
             money_com = re.compile('(\d+)')
             try:
-                area = re.findall(money_com,house_area[item])[0]
-            except:
-                area = 80
-            try:
                 money = int(re.match(money_com,house_price[item]).group(1))
                 if money <= 2000:
-                    money = ((money * 10000) / area)
+                    continue
                 money_list.append(money)
-                # money_dict[house_name[item]].append(money)
             except:
                 pass
         return money_list
         # money_json = json.dumps(money_dict, ensure_ascii = False, indent = 4)
         # return money_json
+
+    def extract_money_area(self,city_name,house_price,house_area):
+        csv_file = open('./data_table/house_price/house_price_%s.csv'%city_name,'w')
+        writers = csv.writer(csv_file)
+        writers.writerow(['house_price','house_price'])
+        house_info = len(house_price)
+        area_list = []
+        for item in range(house_info):
+            money_com = re.compile('(\d+)')
+            try:
+                area = re.findall(money_com, house_area[item])[0]
+            except:
+                area = 100
+            try:
+                money = int(re.match(money_com,house_price[item]).group(1))
+                if money <= 2000:
+                    continue
+            except:
+                continue
+                # money_list.append(money)
+                # area_list.append(area)
+            writers.writerow([money,area])
+        csv_file.close()
+        return area_list
 
     def extract_address(self):
         pass
